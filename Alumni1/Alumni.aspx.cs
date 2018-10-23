57ca76657e7051fcbe22403927c16df97cc171cb
deigant <deigant1998@gmail.com>
2018-10-23 19:05:56 +0530

    alumni registration

--- Alumni1/Alumni.aspx.cs
+++ Alumni1/Alumni.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,9 @@ public partial class Alumni : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        
+            //Label2.BackColor = Color.FromArgb(0, Color.Black);
+      
 
     }
 }
